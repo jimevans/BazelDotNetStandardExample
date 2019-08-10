312efc81988a9ac107b94aa5fcc676fba9e27522
Jim Evans <james.h.evans.jr@gmail.com>
2019-08-10 07:14:22 -0700

    Adding example code to demonstrate issue

---
+++ BazelDotNetStandardExample/ClassLibrary1/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            Uri foo = new Uri("https://bazelbuild.com");
+        }
+    }
+}
